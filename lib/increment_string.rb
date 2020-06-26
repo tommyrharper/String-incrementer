7a48446438fed7d15ffb139b64a1ee55e9483f24
tommyrharper <tommyrharper@gmail.com>
2020-06-26 18:42:32 +0100

    Add conditional to add_zeros method to deal with 0999

--- lib/increment_string.rb
+++ lib/increment_string.rb
@@ -19,7 +19,7 @@ end
 def add_zeros(input_number_string)
   number_of_zeros = input_number_string[/^0+/].size
 
-  if input_number_string.split(//).last(1).join == '0'
+  if input_number_string.split(//).last(1).join == '0' || input_number_string.split(//).last(1).join == '9'
     number_of_zeros -= 1
   end
 
