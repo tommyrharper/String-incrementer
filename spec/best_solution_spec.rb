446680b84ef9a07ff7f605a1ed050c9de09d5975
tommyrharper <tommyrharper@gmail.com>
2020-06-26 19:44:55 +0100

    Add best_solution_spec.rb

---
+++ spec/best_solution_spec.rb
@@ -0,0 +1,28 @@
+require './best_solution.rb'
+
+describe 'increment_string' do
+  it 'returns foo1 for foo' do
+    expect(increment_string('foo')).to eq 'foo1'
+  end
+
+  it 'returns foo2 for foo1' do
+    expect(increment_string('foo1')).to eq 'foo2'
+  end
+
+  it 'returns foo02 for foo01' do
+    expect(increment_string('foo01')).to eq 'foo02'
+  end
+
+  it 'returns foo01 for foo00' do
+    expect(increment_string('foo00')).to eq 'foo01'
+  end
+
+  it 'returns foobar01000 for foobar00999' do 
+    expect(increment_string('foobar00999')).to eq 'foobar01000'
+  end
+
+  it 'returns dcvhvnrwr081 for dcvhvnrwr080' do 
+    expect(increment_string('dcvhvnrwr080')).to eq 'dcvhvnrwr081'
+  end
+
+end
