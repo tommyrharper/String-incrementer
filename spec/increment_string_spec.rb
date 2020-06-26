94f44cd2f043e68300051dd9caf5439b12edd299
tommyrharper <tommyrharper@gmail.com>
2020-06-26 17:44:19 +0100

    Test if foo01 returns foo02

--- spec/increment_string_spec.rb
+++ spec/increment_string_spec.rb
@@ -8,4 +8,8 @@ describe 'increment_string' do
   it 'returns foo2 for foo1' do
     expect(increment_string('foo1')).to eq 'foo2'
   end
+
+  it 'returns foo02 for foo01' do
+    expect(increment_string('foo01')).to eq 'foo02'
+  end
 end
