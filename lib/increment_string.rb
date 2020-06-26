e6133c61d8532f173758497486d0c51671ac6173
tommyrharper <tommyrharper@gmail.com>
2020-06-26 17:44:33 +0100

    Add solution for foo01 returns foo02

--- lib/increment_string.rb
+++ lib/increment_string.rb
@@ -1,6 +1,19 @@
 def increment_string(input)
   input_word = input.gsub(/ *\d+$/, '')
-  input_number = input.scan( /\d+$/ ).first.to_i
-  output_number = input_number + 1
-  output = input_word + output_number.to_s
+  input_number_string = input.scan( /\d+$/ ).first
+  output_number_string = (input_number_string.to_i + 1).to_s
+
+  if input_number_string != nil
+    if input_number_string.itself[0,1] == '0'
+      number_of_zeros = input_number_string[/^0+/].size
+      output_zeros = ''
+      i = 0
+      for i in 1..number_of_zeros
+        output_zeros = output_zeros + '0'
+      end
+      output_number_string = output_zeros + output_number_string
+    end
+  end
+
+  output = input_word + output_number_string
 end
