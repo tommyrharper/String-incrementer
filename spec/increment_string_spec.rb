4b705d24fe4fa2d3a71fa25497f9517dbfda58e6
tommyrharper <tommyrharper@gmail.com>
2020-06-26 17:17:00 +0100

    Add second test foo1 returns foo2

--- spec/increment_string_spec.rb
+++ spec/increment_string_spec.rb
@@ -4,4 +4,8 @@ describe 'increment_string' do
   it 'returns foo1 for foo' do
     expect(increment_string('foo')).to eq 'foo1'
   end
+
+  it 'returns foo2 for foo1' do
+    expect(increment_string('foo1')).to eq 'foo2'
+  end
 end
