13d9e10bda0ccd234b69056a64afd87a28ff331e
tommyrharper <tommyrharper@gmail.com>
2020-06-26 19:00:50 +0100

    Remove old i = 0 statement

--- lib/increment_string.rb
+++ lib/increment_string.rb
@@ -2,11 +2,9 @@ def increment_string(input)
   input_word = input.gsub(/ *\d+$/, '')
   number = input.scan( /\d+$/ ).first
   output_number_string = (number.to_i + 1).to_s
-
   if number_starts_with_zero?(number)
     output_number_string = add_zeros(number) + output_number_string
   end
-
   input_word + output_number_string
 end
 
@@ -18,17 +16,14 @@ end
 
 def add_zeros(number)
   number_of_zeros = number[/^0+/].size
-
   if number.to_i == 0 || number[-1] == '9'
     number_of_zeros -= 1
   end
-
   output_zeros(number_of_zeros)
 end
 
 def output_zeros(number_of_zeros)
   output_zeros = ''
-  i = 0
   for i in 1..number_of_zeros
     output_zeros = output_zeros + '0'
   end
